6cb5a29955972e0a6e4eee19b4eb8e8d0df1354c
P-Pavan <ppavan2002@gmail.com>
2022-08-01 22:00:38 +0530

    Weeeeee

--- Java/Patterns/pattern2.java
+++ Java/Patterns/pattern2.java
@@ -3,8 +3,10 @@ class pattern1 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-
         for(int i=1;i<=x;i++) {
+            for(int j=x-i;j>0;j--) { 
+                System.out.print("  ");
+            }
             for(int j=1;j<=i;j++) { 
                 System.out.print("* ");
             }
---
+++ Java/methods.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class methods {
+    int rollno;
+    String name;
+    float fee;
+    Student(){
+        
+    }
+    public static void main(String args[]) {
+    
+    }    
+}
